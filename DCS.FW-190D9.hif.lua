ec8a7c56e358dc7db0540b4fb5baaf4bfdfa25b6
derammo <817160+derammo@users.noreply.github.com>
2021-09-24 12:15:37 -0400

    implemented navigation light on FW-190D as example

--- DCS.FW-190D9.hif.lua
+++ DCS.FW-190D9.hif.lua
@@ -1,26 +1,9 @@
---[[
-
 function driver.processHighImportance(mainPanelDevice)
     -- called at configured update rate
 
-    -- example for combining/processing arguments:
-    helios.send(2001, string.format(
-            "%0.4f;%0.4f;%0.4f",
-            mainPanelDevice:get_argument_value(220),
-            mainPanelDevice:get_argument_value(219),
-            mainPanelDevice:get_argument_value(218)
-        )
-    )
-
-    -- example for structured indications data:
-    local li = helios.parseIndication(1)
-    if li then
-        helios.send(2002, string.format("%s", helios.ensureString(li.someNamedField1)))
-        helios.send(2003, string.format("%s", helios.ensureString(li.someNamedField2)))
-    end
+    -- EXT_POSITION_LIGHT_LEFT, "External Aircraft Model", "Left Position Light (red)"
+    helios.send(2001, string.format("%d", LoGetAircraftDrawArgumentValue(190) > 0 and 1 or 0))
 end
-
-]]
 
 --[[
 
