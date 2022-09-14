b550ea0b044ce683d7a827e4d198f46adb7a05cb
BlueFinBima <BlueFinBima@users.noreply.github.com>
2022-09-14 21:49:21 +0100

    Change to Helios M2000C Interface and Lua Exports
    Primary change is the handling of most of the text based items which are part of Viewport patches.

---
+++ Helios.M2000C.hif.lua
@@ -0,0 +1,87 @@
+--[[
+
+function driver.processHighImportance(mainPanelDevice)
+    -- called at configured update rate
+
+    -- example for combining/processing arguments:
+    helios.send(2001, string.format(
+            "%0.4f;%0.4f;%0.4f",
+            mainPanelDevice:get_argument_value(220),
+            mainPanelDevice:get_argument_value(219),
+            mainPanelDevice:get_argument_value(218)
+        )
+    )
+
+    -- example for structured indications data:
+    local li = helios.parseIndication(1)
+    if li then
+        helios.send(2002, string.format("%s", helios.ensureString(li.someNamedField1)))
+        helios.send(2003, string.format("%s", helios.ensureString(li.someNamedField2)))
+    end
+end
+
+]]
+
+function driver.processLowImportance(mainPanelDevice)
+    -- PCA_UR structured data
+    li = helios.parseIndication(4) -- 4 for PCA_UR
+    if li then
+        helios.send(2060, string.format("%-3s%-3s%-3s%-3s%-3s", helios.ensureString(li.PCA_LCD_1_0), helios.ensureString(li.PCA_LCD_1_1), helios.ensureString(li.PCA_LCD_1_2), helios.ensureString(li.PCA_LCD_1_3), helios.ensureString(li.PCA_LCD_1_4)))
+    end
+    -- PCA_BR structured data
+    li = helios.parseIndication(5) -- 5 for PCA_BR
+    if li then
+        helios.send(2061, string.format("%-3s%-3s%-3s%-3s%-3s", helios.ensureString(li.PCA_LCD_2_0), helios.ensureString(li.PCA_LCD_2_1), helios.ensureString(li.PCA_LCD_2_2), helios.ensureString(li.PCA_LCD_2_3), helios.ensureString(li.PCA_LCD_2_4)))
+    end
+    -- VHF Comm structured data
+    li = helios.parseIndication(8)
+    if li then
+        helios.send(2062, string.format("%-8s", helios.ensureString(li.text_COM_VHF)))
+    end
+    -- UHF Comm structured data
+    li = helios.parseIndication(7)
+    if li then
+        helios.send(2063, string.format("%-8s", helios.ensureString(li.text_COM_UHF1)))
+        helios.send(2064, string.format("%-8s", helios.ensureString(li.text_COM_UHF2)))
+    end
+    -- PPA structured data
+    li = helios.parseIndication(6)
+    if li then
+        helios.send(2065, string.format("%-2s", helios.ensureString(li.text_PPA_QTY)))
+        helios.send(2066, string.format("%-2s", helios.ensureString(li.text_PPA_INT)))
+    end
+    -- Fuel structured data
+    li = helios.parseIndication(3)
+    if li then
+        helios.send(2067, string.format("%-3s", helios.ensureString(li.txt_fuel_g)))
+    end
+    -- PCN U structured data
+    li = helios.parseIndication(9)
+    if li then
+        helios.send(2068, string.format("%6s", helios.ensureString(li.PCN_UL_DIGITS)))
+        helios.send(2069, string.format("%6s", helios.ensureString(li.PCN_UL_POINTS)))
+        helios.send(2070, string.format("%6s", helios.ensureString(li.PCN_UR_DIGITS)))
+        helios.send(2071, string.format("%6s", helios.ensureString(li.PCN_UR_POINTS)))
+        helios.send(2072, string.format("%1d", li["PCN_UL_N"] and 1 or 0))
+        helios.send(2073, string.format("%1d", li["PCN_UL_S"] and 1 or 0))
+        helios.send(2074, string.format("%1d", li["PCN_UR_E"] and 1 or 0))
+        helios.send(2075, string.format("%1d", li["PCN_UR_W"] and 1 or 0))
+        helios.send(2076, string.format("%1d", li["PCN_UL_P"] and 1 or 0))
+        helios.send(2077, string.format("%1d", li["PCN_UL_M"] and 1 or 0))
+        helios.send(2078, string.format("%1d", li["PCN_UR_P"] and 1 or 0))
+        helios.send(2079, string.format("%1d", li["PCN_UR_M"] and 1 or 0))
+    end    
+    -- PCN B structured data
+    li = helios.parseIndication(10)
+    if li then
+        helios.send(2080, string.format("%s", helios.ensureString(li.PCN_BL_DIGITS)))
+        helios.send(2081, string.format("%s", helios.ensureString(li.PCN_BR_DIGITS)))
+    end
+    -- EVF structured data
+    li = helios.parseIndication(11)
+    if li then
+        helios.send(2082, string.format("%-2s", helios.ensureString(li["evf-digits"])))
+    end
+
+end
+
