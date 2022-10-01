5733ae4b30f1d458ab3823dd73534ba6b27f2e98
BlueFinBima <BlueFinBima@users.noreply.github.com>
2022-10-01 18:06:13 +0100

    Changes to the Tucano interface by Soyouz2000
    Multiple changes to the Tucano interface by Soyouz2000 with associated changes to the exports.

--- DCS.A-29B.hif.lua
+++ DCS.A-29B.hif.lua
@@ -1,26 +1,15 @@
---[[
-
 function driver.processHighImportance(mainPanelDevice)
     -- called at configured update rate
+        helios.send(2001, string.format("%0.1f", mainPanelDevice:get_argument_value(226))) -- Light MainPump L
+        helios.send(2002, string.format("%0.1f", mainPanelDevice:get_argument_value(227))) -- Light MainPump R
+        helios.send(2003, string.format("%0.1f", mainPanelDevice:get_argument_value(228))) -- Light AuxPump L
+        helios.send(2004, string.format("%0.1f", mainPanelDevice:get_argument_value(229))) -- Light AuxPump R
+        helios.send(2005, string.format("%0.1f", mainPanelDevice:get_argument_value(230))) -- Light OVRD L
+        helios.send(2006, string.format("%0.1f", mainPanelDevice:get_argument_value(231))) -- Light OVRD R
+        helios.send(2007, string.format("%0.1f", mainPanelDevice:get_argument_value(232))) -- Light VNTRL
+        helios.send(2008, string.format("%0.4f", mainPanelDevice:get_argument_value(80))) -- Throttle Position Lock
 
-    -- example for combining/processing arguments:
-    helios.send(2001, string.format(
-            "%0.4f;%0.4f;%0.4f",
-            mainPanelDevice:get_argument_value(220),
-            mainPanelDevice:get_argument_value(219),
-            mainPanelDevice:get_argument_value(218)
-        )
-    )
-
-    -- example for structured indications data:
-    local li = helios.parseIndication(1)
-    if li then
-        helios.send(2002, string.format("%s", helios.ensureString(li.someNamedField1)))
-        helios.send(2003, string.format("%s", helios.ensureString(li.someNamedField2)))
-    end
 end
-
-]]
 
 --[[
 
