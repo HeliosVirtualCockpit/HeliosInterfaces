6e2091bb39681895eccac8f80a4914169273210f
BlueFinBima <BlueFinBima@users.noreply.github.com>
2024-04-02 18:02:49 +0100

    Change for A-10C CDU Brightness Rocker
    This is the Interfaces submodule fix for https://github.com/HeliosVirtualCockpit/Helios/issues/819 and the commit ID is the one prior to the actual commit for the fix in the c# interface

---
+++ Helios.A10C.hif.lua
@@ -0,0 +1,22 @@
+-- Exports.Lua from Helios A-10C Interface
+function driver.processHighImportance(mainPanelDevice)
+	-- Send Altimeter Values	
+	helios.send(2051, string.format("%0.4f;%0.4f;%0.5f", mainPanelDevice:get_argument_value(52), mainPanelDevice:get_argument_value(53), mainPanelDevice:get_argument_value(51)))
+	helios.send(2059, string.format("%0.2f;%0.2f;%0.2f;%0.3f", mainPanelDevice:get_argument_value(56), mainPanelDevice:get_argument_value(57), mainPanelDevice:get_argument_value(58), mainPanelDevice:get_argument_value(59)))		
+	-- Calcuate HSI Value
+	helios.send(2029, string.format("%0.2f;%0.2f;%0.4f", mainPanelDevice:get_argument_value(29), mainPanelDevice:get_argument_value(30), mainPanelDevice:get_argument_value(31)))
+	-- Calculate Total Fuel
+	helios.send(2090, string.format("%0.2f;%0.2f;%0.5f", mainPanelDevice:get_argument_value(90), mainPanelDevice:get_argument_value(91), mainPanelDevice:get_argument_value(92)))
+	-- instruments pure values												  
+	--helios.send(3012, string.format("%0.3f", mainPanelDevice:get_argument_value(12)))
+end
+
+function driver.processLowImportance(mainPanelDevice)
+	-- Get Radio Frequencies
+	local lUHFRadio = GetDevice(54)
+	helios.send(2000, string.format("%7.3f", lUHFRadio:get_frequency()/1000000))
+	-- ILS Frequency
+	--helios.send(2251, string.format("%0.1f;%0.1f", mainPanelDevice:get_argument_value(251), mainPanelDevice:get_argument_value(252)))
+	-- TACAN Channel
+	helios.send(2263, string.format("%0.2f;%0.2f;%0.2f", mainPanelDevice:get_argument_value(263), mainPanelDevice:get_argument_value(264), mainPanelDevice:get_argument_value(265)))
+end
