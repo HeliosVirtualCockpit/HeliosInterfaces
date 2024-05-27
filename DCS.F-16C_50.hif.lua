08bff9ddcaa4f262ea66b41c378a0b32dca768d0
BlueFinBima <BlueFinBima@users.noreply.github.com>
2024-05-27 09:39:48 +0100

    Fix for #17 in the F-16C_50 interface
    Fix for #17 in the F-16C_50 interface which had duplicate IDs for 184 and 185.  Also sorted out ejection handle and "UNKNOWN" duplicates.

--- DCS.F-16C_50.hif.lua
+++ DCS.F-16C_50.hif.lua
@@ -1,5 +1,5 @@
+-- Helios functions for F-16C_50 1.6.609.2
 --[[
-
 function driver.processHighImportance(mainPanelDevice)
     -- called at configured update rate
 
@@ -19,13 +19,13 @@ function driver.processHighImportance(mainPanelDevice)
         helios.send(2003, string.format("%s", helios.ensureString(li.someNamedField2)))
     end
 end
-
 ]]
-
---[[
-
 function driver.processLowImportance(mainPanelDevice) --luacheck: no unused args
     -- same things as processHighImportance can be done here, called a few times per second at most
+	
+	-- 184 and 185 are required for multiple interface items, so they are sent under alias id's
+	helios.send(2184, string.format("%1d", mainPanelDevice:get_argument_value(184)))
+    helios.send(2185, string.format("%1d", mainPanelDevice:get_argument_value(185)))
+	helios.send(3184, string.format("%1d", mainPanelDevice:get_argument_value(184)))
+    helios.send(3185, string.format("%1d", mainPanelDevice:get_argument_value(185)))
 end
-
-]]
