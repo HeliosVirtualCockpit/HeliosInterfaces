bbcbe8a31db44556b550834a5b8e852fc89c9315
BlueFinBima <BlueFinBima@users.noreply.github.com>
2026-06-11 14:00:32 +0100

    Update for UH-60L community Mod Interface
    Now requires patches to allow the barcode date to be identified!
    Added args 100 and 103 for IAS and VVI

--- Helios.UH60L.hif.lua
+++ Helios.UH60L.hif.lua
@@ -1,9 +1,12 @@
 -- Exports.Lua from Helios UH-60L interface
--- Version: 1.6.6160.0000
--- Commit: f114afb9621c70d0211b82d84cb4664ab140dec4
+-- Version: 1.6.6190.0000
+-- Commit: 3ab7cb14cd15532aefc5eb573f816a0a0e2fd63a
+--
+-- ** This code requires patches to be applied to the UH-60L module in order to identify exported data **
+--
 
 driver.state = {}
-
+driver.NoPatchesMessageAlreadySent = false
 function driver.processHighImportance(mainPanelDevice)
 
 	-- Pilot VSI ball combined movement values
@@ -75,208 +78,135 @@ function driver.processHighImportance(mainPanelDevice)
     )
 end
 
-driver.UH60Lindicator = {{{0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,1350,1400,1450},
-							{0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,1350,1400,1450},
-							{-50,-40,-30,-20,-10,0,10,20,30,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,130,140,150,160},
-							{0,5,10,15,20,25,30,32.5,35,37.5,40,42.5,45,47.5,50,52.5,55,57.5,60,62.5,65,67.5,70,80,90,100,110,130,150,170},
-							{-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,75,80,85,90,95,100,105,110,115,120,125,130,135,150,160,170},
-							{-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,75,80,85,90,95,100,105,110,115,120,125,130,135,150,160,170},
-							{10,15,20,25,30,35,40,42.5,45,47.5,50,52.5,55,57.5,60,62.5,65,67.5,70,72.5,75,77.5,80,82.5,85,87.5,90,100,110,120},
-							{10,15,20,25,30,35,40,42.5,45,47.5,50,52.5,55,57.5,60,62.5,65,67.5,70,72.5,75,77.5,80,82.5,85,87.5,90,100,110,120},
-							{0,50,100,150,200,250,300,350,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925},
-							{0,50,100,150,200,250,300,350,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925},
-							{0,10,20,30,40,45,50,55,60,65,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102,104,106,108},
-							{0,10,20,30,40,45,50,55,60,65,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102,104,106,108}},
-							{{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29}},
-							{{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29},
-							{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29}}
-							}
+driver.UH60Lindicator = {
+                            {
+                                {0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,1350,1400,1450,1500},
+							    {0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,1350,1400,1450,1500},
+							    {-50,-40,-30,-20,-10,0,10,20,30,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,130,140,150,160},
+							    {0,5,10,15,20,25,30,32.5,35,37.5,40,42.5,45,47.5,50,52.5,55,57.5,60,62.5,65,67.5,70,80,90,100,110,130,150,170},
+							    {-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,75,80,85,90,95,100,105,110,115,120,125,130,135,150,160,170},
+							    {-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,75,80,85,90,95,100,105,110,115,120,125,130,135,150,160,170},
+							    {10,15,20,25,30,35,40,42.5,45,47.5,50,52.5,55,57.5,60,62.5,65,67.5,70,72.5,75,77.5,80,82.5,85,87.5,90,100,110,120},
+							    {10,15,20,25,30,35,40,42.5,45,47.5,50,52.5,55,57.5,60,62.5,65,67.5,70,72.5,75,77.5,80,82.5,85,87.5,90,100,110,120},
+							    {0,50,100,150,200,250,300,350,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925},
+							    {0,50,100,150,200,250,300,350,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925},
+							    {0,10,20,30,40,45,50,55,60,65,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102,104,106,108},
+							    {0,10,20,30,40,45,50,55,60,65,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102,104,106,108}
+                            },
+							{
+                                {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29}
+                            },
+							{
+                                {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29},
+							    {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29}
+                            }
+                       }
+
 driver.displayNames = {"TotalFuel","TGT1","TGT2","Ng1","Ng2","Torque1","Torque2"}
+driver.barNames = {
+					{"FUEL_L", "FUEL_R", "XMSNTEMP", "XMSNPRESS", "E1OILTEMP", "E2OILTEMP", "E1PRESS", "E2PRESS", "TGT1", "TGT2", "NG1", "NG2"},
+					{"PLT_E1NP", "PLT_RRPM", "PLT_E2NP", "PLT_E1TRQ", "PLT_E2TRQ"},
+					{"CPLT_E1NP", "CPLT_RRPM", "CPLT_E2NP", "CPLT_E1TRQ", "CPLT_E2TRQ"}
+				  }
 driver.state.indications = {}
+function lprint(s)
+	log.write("Helios.Export", log.INFO, s)
+end
 function driver.processLowImportance(mainPanelDevice)
 local li
 	do -- constrain variable scope
-		local results={}
 		-- Indicator 0 for the Center panel bar graphs and numeric displays
-		li = list_indication(0)
+		li = helios.parseIndication(0)
 		if li ~= driver.state.indications[0] then
-			local currentGaugeNumber = 1
-			local currentSegment = 1
-			local segNextValue = 9999
-			local segCursor = nil
-			local segStart = 0
-			local segNumberStart = 0
-			local displayNum = 0
-
-			--[[
-			We need to pull out twelve bar graphs and 5 numeric displays (the easy bit).  There appears to be no delimiter
-			between the bar graphs, and several of them have non-linear scales which poses an additional challenge so we use
-			a table based approach to identify the specific gauge.  
-			All of the temperatures start at their origins.
-			This routine relies on the bar graphs all appearing in the same order!
-			]]
-
-			local m = li:gmatch("-----------------------------------------\n([^\n]+)\n([^\n]*)\n")
-			
-			while true do
-				local name, value = m()
-				if not name then
-					results["gauge" .. currentGaugeNumber] = string.format("%s;%s;%s;%s", segStart, segCursor, segNumberStart, currentSegment)	
-					break 
+			if li["FuelAmount"] == nil then 
+				if driver.NoPatchesMessageAlreadySent == false then
+					log.write("Helios.Export",log.ERROR,"Driver: UH-60L - Patches appear not to be applied to module.")
+					driver.NoPatchesMessageAlreadySent = true
 				end
-				local seg = name:gsub("segment_","")
-				local segNumeric
-
-				if name == seg then
-					-- non segment ie numeric display data
-					if value ~= "" then
-						displayNum = displayNum + 1
-						results[driver.displayNames[displayNum]] = value
-					end
-				else
-					-- process a segment entry
-					segNumeric = tonumber(seg)
-					if segCursor == nil then  	-- first time processing a segement
-						segStart = segNumeric 	-- we must be a start entry
-						segCursor = segStart	-- cursor set to the first segement
-						segNextValue = segStart
-					end
-					if segStart == segCursor and segNextValue == segStart then
-						for currSegment = 1,30,1 do
-							if segNumeric == driver.UH60Lindicator[1][currentGaugeNumber][currSegment] then
-								segNumberStart = currSegment
-								segNextValue = driver.UH60Lindicator[1][currentGaugeNumber][currSegment+1]
-								currentSegment = currSegment
-								break
-							end				
+			else
+				for j = 1,#driver.barNames[1],1 do
+					local n = {}
+					for k, v in pairs(li) do
+						if k:find(driver.barNames[1][j].."_segment_") ~= nil then
+							local a = k:gsub(driver.barNames[1][j].."_segment_([%d.-]+)%s*","%1")
+							table.insert(n, tonumber(a))
 						end
-					else
-						if segNumeric == segNextValue then
-							currentSegment = currentSegment + 1
-							segNextValue = driver.UH60Lindicator[1][currentGaugeNumber][currentSegment+1]					
-							segCursor = segNumeric
-						else
-							results["gauge" .. currentGaugeNumber] = string.format("%s;%s;%s;%s", segStart, segCursor, segNumberStart, currentSegment)
-							currentGaugeNumber = currentGaugeNumber + 1	
-							for currSegment = 1,30,1 do
-								if segNumeric == driver.UH60Lindicator[1][currentGaugeNumber][currSegment] then 
-									segNumberStart = currSegment
-									segNextValue = driver.UH60Lindicator[1][currentGaugeNumber][currSegment+1]
-									currentSegment = currSegment
-									break
-								end				
+					end
+					if n ~= nil then
+						table.sort(n)
+						local startVal, endVal, startSeg, endSeg
+						startVal = n[1]
+						endVal = n[#n]
+						for m = 1, #driver.UH60Lindicator[1][j] do
+							if driver.UH60Lindicator[1][j][m] == n[1] then
+								startSeg = m
 							end
-							segStart = segNumeric
-							segCursor = segStart					
+							if driver.UH60Lindicator[1][j][m] == n[#n] then
+								endSeg = m
+								break
+							end
 						end
-					end			
+						if startVal == nil or endVal== nil or startSeg == nil or endSeg == nil then 
+							-- lprint(string.format("Nil Value from %s;%s;%s;%s;%s", driver.barNames[1][j], helios.ensureString(startVal), helios.ensureString(endVal), helios.ensureString(startSeg), helios.ensureString(endSeg)))
+							helios.send(2064+j,"0;0;0;0") -- bar gauge						
+						else
+							helios.send(2064+j,string.format(string.format("%.1f;%.1f;%d;%s", startVal, endVal, startSeg, endSeg))) -- bar gauge
+						end
+					end
 				end
+				helios.send(2060,string.format("%s", helios.ensureString(li["FuelAmount"])))
+				helios.send(2061,string.format("%s", helios.ensureString(li["E1TGT"])))
+				helios.send(2062,string.format("%s", helios.ensureString(li["E2TGT"])))
+				helios.send(2063,string.format("%s", helios.ensureString(li["E1NG"])))
+				helios.send(2064,string.format("%s", helios.ensureString(li["E2NG"])))		
 			end
-
-			helios.send(2060,string.format("%s", helios.ensureString(results["TotalFuel"])))
-			helios.send(2061,string.format("%s", helios.ensureString(results["TGT1"])))
-			helios.send(2062,string.format("%s", helios.ensureString(results["TGT2"])))
-			helios.send(2063,string.format("%s", helios.ensureString(results["Ng1"])))
-			helios.send(2064,string.format("%s", helios.ensureString(results["Ng2"])))
-			for ii=1,12,1 do
-				if results["gauge"..ii] then
-					helios.send(2064+ii,string.format("%s", helios.ensureString(results["gauge"..ii]))) -- bar gauge
-				end
-			end
-			driver.state.indications[0] = li
+			driver.state.indications[0] = li			
 		end
 	end
 	
-	for jj= 1,2,1 do
-		local results={}
+	for jj= 2,3,1 do
 		-- Indicator 1 and 2 for the RPM and Torque bar graphs and numeric displays for Pilot and Copilot
-		li = list_indication(jj)
-		if li ~= driver.state.indications[jj] then
-
-			local currentGaugeNumber = 1
-			local currentSegment = 1
-			local segNextValue = 9999
-			local segCursor = nil
-			local segStart = 0
-			local segNumberStart = 0
-			local displayNum = 5
-
-			--[[
-			We need to pull out five bar graphs and 2 numeric displays.  There appears to be no delimiter
-			between the bar graphs so we use a table based approach to identify the specific gauge.  
-			This routine relies on the bar graphs all appearing in the same order!
-			]]
-
-			local m = li:gmatch("-----------------------------------------\n([^\n]+)\n([^\n]*)\n")
-			
-			while true do
-				local name, value = m()
-				if not name then
-					results["gauge" .. currentGaugeNumber] = string.format("%s;%s;%s;%s", segStart, segCursor, segNumberStart, currentSegment)	
-					break 
+		li = helios.parseIndication(jj-1)
+		if li ~= driver.state.indications[jj] and li ~= nil then		
+			for j = 1,#driver.barNames[jj],1 do
+				local n = {}
+				for k, v in pairs(li) do
+					if k:find(driver.barNames[jj][j].."_segment_") ~= nil then
+						local a = k:gsub(driver.barNames[jj][j].."_segment_([%d.-]+)%s*","%1")
+						table.insert(n, tonumber(a))
+					end
 				end
-				local seg = name:gsub("segment_","")
-				local segNumeric
-
-				if name == seg then
-					-- non segment ie numeric display data
-					if value ~= "" then
-						displayNum = displayNum + 1
-						results[driver.displayNames[displayNum]] = value
-					end
-				else
-					-- process a segment entry
-					segNumeric = tonumber(seg)
-					if segCursor == nil then  	-- first time processing a segement
-						segStart = segNumeric 	-- we must be a start entry
-						segCursor = segStart	-- cursor set to the first segement
-						segNextValue = segStart
-					end
-					if segStart == segCursor and segNextValue == segStart then
-						for currSegment = 1,30,1 do
-							if segNumeric == driver.UH60Lindicator[jj+1][currentGaugeNumber][currSegment] then
-								segNumberStart = currSegment
-								segNextValue = driver.UH60Lindicator[jj+1][currentGaugeNumber][currSegment+1]
-								currentSegment = currSegment
-								break
-							end				
+				if n ~= nil then
+					table.sort(n)
+					local startVal, endVal, startSeg, endSeg
+					startVal = n[1]
+					endVal = n[#n]
+					for m = 1, #driver.UH60Lindicator[jj][j] do
+						if driver.UH60Lindicator[jj][j][m] == n[1] then
+							startSeg = m
 						end
+						if driver.UH60Lindicator[jj][j][m] == n[#n] then
+							endSeg = m
+							break
+						end
+					end
+					if startVal == nil or endVal== nil or startSeg == nil or endSeg == nil then 
+						-- lprint(string.format("Nil Value from %s;%s;%s;%s;%s", driver.barNames[jj][j], helios.ensureString(startVal), helios.ensureString(endVal), helios.ensureString(startSeg), helios.ensureString(endSeg)))
+						helios.send(2078+((jj-2)*7)+j,"0;0;0;0") -- bar gauge						
 					else
-						if segNumeric == segNextValue then
-							currentSegment = currentSegment + 1
-							segNextValue = driver.UH60Lindicator[jj+1][currentGaugeNumber][currentSegment+1]					
-							segCursor = segNumeric
-						else
-							results["gauge" .. currentGaugeNumber] = string.format("%s;%s;%s;%s", segStart, segCursor, segNumberStart, currentSegment)
-							currentGaugeNumber = currentGaugeNumber + 1	
-							for currSegment = 1,30,1 do
-								if segNumeric == driver.UH60Lindicator[jj+1][currentGaugeNumber][currSegment] then 
-									segNumberStart = currSegment
-									segNextValue = driver.UH60Lindicator[jj+1][currentGaugeNumber][currSegment+1]
-									currentSegment = currSegment
-									break
-								end				
-							end
-							segStart = segNumeric
-							segCursor = segStart					
-						end
-					end			
+						helios.send(2078+((jj-2)*7)+j,string.format("%.1f;%.1f;%d;%s", startVal, endVal, startSeg, endSeg)) -- bar gauge
+					end
 				end
 			end
-			helios.send(2077+((jj-1)*7),string.format("%s", helios.ensureString(results["Torque1"])))
-			helios.send(2078+((jj-1)*7),string.format("%s", helios.ensureString(results["Torque2"])))
-			for ii=1,5,1 do
-				if results["gauge"..ii] then
-					helios.send(2078+((jj-1)*7)+ii,string.format("%s", helios.ensureString(results["gauge"..ii]))) -- bar gauge
-				end
-			end
+			helios.send(2077+((jj-2)*7),string.format("%s", helios.ensureString(li["E1TRQ_Text"])))
+			helios.send(2078+((jj-2)*7),string.format("%s", helios.ensureString(li["E2TRQ_Text"])))			
 			driver.state.indications[jj] = li
 		end
 	end
@@ -331,7 +261,11 @@ local li
 				end
 				if value ~= "" and value ~= ":" then
 					if  currentLine < 3 then
-						results["Line"..currentLine]= string.format("%02d",value)
+						if type(value) == "number" then 
+							results["Line"..currentLine]= string.format("%02d",value)
+						else
+							results["Line"..currentLine]= "  "
+						end
 						currentLine = currentLine + 1
 					else
 						if currentLine == 3 then
